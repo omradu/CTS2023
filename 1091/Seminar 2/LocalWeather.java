a332fe69d4874ffbfb86fd71575aef28c443bae5
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-02-26 09:50:22 +0200

    Create LocalWeather.java

---
+++ 1091/Seminar 2/LocalWeather.java
@@ -0,0 +1,22 @@
+class LocalWeather{
+    // constructor
+    public LocalWeather() {}
+
+    // get current temperature in local area
+    public int getCurrentTemp() {
+        // get user lattitude and longtitude
+        double latitude = getLatitude();
+        double longitude = getLongitude();
+        // find and return current temperature using latitude and longtitude
+        //TO DO
+        return 0;
+    }
+    public double getLatitude() {
+        // return user latitiude
+        return 0;
+    }
+    public double getLongitude() {
+        // return user longitude
+        return 0;
+    }
+}
