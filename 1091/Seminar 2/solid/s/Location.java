c219ff93d77dfcfe192708eb225c029130d2df4c
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-02-29 19:51:11 +0200

    Create Location.java

---
+++ 1091/Seminar 2/solid/s/Location.java
@@ -0,0 +1,13 @@
+class Location {
+  
+    int latitude;
+    int longitude;
+    
+    public double getLatitude() {
+        return latitude;
+    }
+    
+    public double getLongitude() {
+        return longitude;
+    }
+}
