e216492064d083a7dce648ea695629dee89222e0
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-03-05 15:23:44 +0200

    Create Eagle.java

---
+++ 1104/Lab 3/solid/i/correct/Eagle.java
@@ -0,0 +1,19 @@
+public class Eagle implements FeatheredCreature, FlyingCreature{
+    String currentLocation;
+    int numberOfFeathers;
+
+    public Eagle(int initialNumberOfFeathers) {
+
+        this.numberOfFeathers = initialNumberOfFeathers;
+    }
+
+    public void fly() {
+
+        this.currentLocation = "in the air";
+    }
+
+    public void molt() {
+
+        this.numberOfFeathers -= 1;
+    }
+}
