fbfff2ad22583989549741dbaa39c190c4a31cae
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-03-23 12:52:19 +0200

    Create StoneBlock.java

---
+++ 1091/Seminar 5/designpatterns/prototype/StoneBlock.java
@@ -0,0 +1,21 @@
+package designpatterns.prototype;
+
+public class StoneBlock extends Block{
+    String type;
+
+    public StoneBlock() {
+        type = "Stone Block";
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        StoneBlock clone = (StoneBlock) super.clone();
+        if(type!=null) {
+            clone.type = type;
+        } else {
+            clone.type = null;
+        }
+
+        return clone;
+    }
+}
