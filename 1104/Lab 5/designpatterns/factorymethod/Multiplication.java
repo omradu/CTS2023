60b541802ca86e474c81afa86835ffb268a1d52e
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-03-23 12:57:28 +0200

    Create Multiplication.java

---
+++ 1104/Lab 5/designpatterns/factorymethod/Multiplication.java
@@ -0,0 +1,8 @@
+package dp.factorymethod;
+
+public class Multiplication implements BinaryOperator{
+    @Override
+    public double apply(double number1, double number2) {
+        return number1 * number2;
+    }
+}
