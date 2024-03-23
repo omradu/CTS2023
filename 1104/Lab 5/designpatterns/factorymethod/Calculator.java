8784a62f1fbb86df3b5e94e64628be8574407590
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-03-23 12:56:26 +0200

    Create Calculator.java

---
+++ 1104/Lab 5/designpatterns/factorymethod/Calculator.java
@@ -0,0 +1,38 @@
+package dp.factorymethod;
+
+public class Calculator {
+    private double number1;
+    private double number2;
+    private BinaryOperator binaryOperator;
+
+    public Calculator(double number1, double number2, String operation) {
+        this.number1 = number1;
+        this.number2 = number2;
+
+        BinaryOperatorFactory binaryOperatorFactory;
+
+        if("+".equals(operation)) {
+            binaryOperatorFactory = new AdditionFactory();
+        } else if("-".equals(operation)) {
+            binaryOperatorFactory = new SubstractionFactory();
+        } else if("*".equals(operation)) {
+            binaryOperatorFactory = new MultiplicationFactory();
+        } else if("/".equals(operation)) {
+            binaryOperatorFactory = new DivisionFactory();
+        } else {
+            throw new UnsupportedOperationException();
+        }
+
+        binaryOperator = binaryOperatorFactory.getBinaryOperator();
+    }
+
+    public double calculate() {
+        try {
+            return binaryOperator.apply(number1,number2);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return 0;
+    }
+}
