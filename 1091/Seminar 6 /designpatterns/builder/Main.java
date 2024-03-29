ba0fdad0934de805ba22ec562579e9198f668d9a
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-03-29 08:39:38 +0200

    Create Main.java

---
+++ 1091/Seminar 6 /designpatterns/builder/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Film film = new Film.FilmBuilder("Babrie", 2023).addBileteVandute(23000).addRatingIMDB(7).esteDisponibilIn4D().build();
+        Film2 film2 = new Film2.FilmBuilder2("Avatar", 2009).addGen("actiune").addDurataInMinute(100).build();
+
+        Director director = new Director(new FilmBuilder3("Dune 2", 2024));
+        director.build();
+        Film film3 = director.getFilm();
+
+    }
+}
