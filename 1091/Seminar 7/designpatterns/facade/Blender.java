44d4ee8b2ed7163720f7279308b4dc53632f6a1a
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-04-20 00:29:09 +0300

    Create Blender.java

---
+++ 1091/Seminar 7/designpatterns/facade/Blender.java
@@ -0,0 +1,11 @@
+public class Blender {
+    protected boolean estePornit = false;
+
+    public void porneste() {
+        estePornit = true;
+    }
+
+    public void opreste() {
+        estePornit = false;
+    }
+}
