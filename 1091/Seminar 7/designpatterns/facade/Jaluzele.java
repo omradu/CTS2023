87795cdb3c70374c46a8bcacafcc288499bf86cb
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-04-20 00:29:32 +0300

    Create Jaluzele.java

---
+++ 1091/Seminar 7/designpatterns/facade/Jaluzele.java
@@ -0,0 +1,11 @@
+public class Jaluzele {
+    protected boolean suntDeschise = false;
+
+    public void deschide() {
+        suntDeschise = true;
+    }
+
+    public void inchide() {
+        suntDeschise = false;
+    }
+}
