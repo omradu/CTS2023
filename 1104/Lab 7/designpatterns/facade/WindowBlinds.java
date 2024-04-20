e55817645253ed29388f262df2ebaa4117483d1f
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-04-20 23:23:25 +0300

    Create WindowBlinds.java

---
+++ 1104/Lab 7/designpatterns/facade/WindowBlinds.java
@@ -0,0 +1,11 @@
+public class WindowBlinds {
+    protected boolean areOpen = false;
+
+    public void open() {
+        areOpen = true;
+    }
+
+    public void close() {
+        areOpen = false;
+    }
+}
