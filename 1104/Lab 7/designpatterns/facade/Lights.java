72d8797e367b4860562286463823f731bbf32060
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-04-20 23:21:51 +0300

    Create Lights.java

---
+++ 1104/Lab 7/designpatterns/facade/Lights.java
@@ -0,0 +1,11 @@
+public class Lights {
+    protected boolean areON = false;
+
+    public void turnOn() {
+        areON = true;
+    }
+
+    public void turnOff() {
+        areON = false;
+    }
+}
