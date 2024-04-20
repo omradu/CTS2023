64a508c7136dcf956503ec2e2de4a0a64f216215
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-04-20 23:22:22 +0300

    Create TV.java

---
+++ 1104/Lab 7/designpatterns/facade/TV.java
@@ -0,0 +1,13 @@
+public class TV {
+    protected boolean isOn = false;
+
+    public void turnOn() {
+
+        isOn = true;
+    }
+
+    public void turnOff() {
+
+        isOn = false;
+    }
+}
