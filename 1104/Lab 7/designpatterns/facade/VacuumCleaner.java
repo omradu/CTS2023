864770ca6fbe91138e5700afbbcc75f185519fce
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-04-20 23:23:00 +0300

    Create VacuumCleaner.java

---
+++ 1104/Lab 7/designpatterns/facade/VacuumCleaner.java
@@ -0,0 +1,11 @@
+public class VacuumCleaner {
+    protected boolean isOn = false;
+
+    public void turnOn() {
+        isOn = true;
+    }
+
+    public void turnOff() {
+        isOn = false;
+    }
+}
