ac269e202cd2e245f2e25a04c8156329ddaee6c7
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-04-26 15:14:30 +0300

    Create Main.java

---
+++ 1104/Lab  9/designpatterns/chain/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        ManagerM1 M1 = new ManagerM1();
+        ManagerM2 M2 = new ManagerM2();
+        VicePresident VP = new VicePresident();
+
+        M1.setNextApprover(M2);
+        M2.setNextApprover(VP);
+
+        M1.approveBudget(150);
+        System.out.println("------------------------");
+        M1.approveBudget(10000);
+    }
+}
