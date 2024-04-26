199bed3442838cba5c0f01deb7bce1c88d3f02b4
Maria Radu <68914986+omradu@users.noreply.github.com>
2024-04-26 15:12:08 +0300

    Create Main.java

---
+++ 1104/Lab  9/designpatterns/flyweight/Main.java
@@ -0,0 +1,34 @@
+package server.structurale.flyweight;
+
+public class Main {
+    private static Character[] textCharacters = new Character[100];
+    private static CharacterContext[] charactersLocation = new CharacterContext[100];
+    private static int nbCharacters = 0;
+
+    public static void pressKey(String character, int location, CharacterFactory characterFactory) {
+        textCharacters[nbCharacters] = characterFactory.getCharacter(character);
+        charactersLocation[nbCharacters] = new CharacterContext(location);
+        nbCharacters++;
+    }
+
+    public static void main(String[] args) {
+
+        CharacterFactory characterFactory = new CharacterFactory();
+        pressKey("A", nbCharacters, characterFactory);
+        pressKey("n", nbCharacters, characterFactory);
+        pressKey("a", nbCharacters, characterFactory);
+        pressKey("a", nbCharacters, characterFactory);
+        pressKey("r", nbCharacters, characterFactory);
+        pressKey("e", nbCharacters, characterFactory);
+        pressKey("m", nbCharacters, characterFactory);
+        pressKey("e", nbCharacters, characterFactory);
+        pressKey("r", nbCharacters, characterFactory);
+        pressKey("e", nbCharacters, characterFactory);
+        
+
+        for (int i=0; i<nbCharacters; i++) {
+            textCharacters[i].print(charactersLocation[i]);
+        }
+
+    }
+}
